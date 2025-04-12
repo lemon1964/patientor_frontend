0d62e381a84cd75fea3ea5819b0cc25f46ce8a05
lemon <lemon@example.com>
2025-04-13 01:26:06 +0300

    no code

--- src/constants.ts
+++
@@ -1,8 +0,0 @@
-// Локально
-// export const apiBaseUrl = 'http://localhost:3001/api';
-
-// Рендер и локально
-// export const apiBaseUrl = '/api';
-
-// // Vercel, Рендер и локально
-export const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
